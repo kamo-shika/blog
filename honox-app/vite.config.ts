de1a53531e0aa12cf554cdc746280b2b17136484
firstYearEmployment <forwork07241998@gmail.com>
2025-01-02 00:47:50 +0900

    ✨ MDKの導入
    MDKを依存関係、型定義を追加する

--- honox-app/vite.config.ts
+++ honox-app/vite.config.ts
@@ -1,11 +1,12 @@
 import honox from 'honox/vite'
 import ssg from '@hono/vite-ssg'
+import mdx from '@mdx-js/rollup'
 import { defineConfig } from 'vite'
  
 const entry = './app/server.ts'
  
 export default defineConfig(() => {
   return {
-    plugins: [honox(), ssg({ entry })]
+    plugins: [honox(), ssg({ entry }), mdx()]
   }
 })
