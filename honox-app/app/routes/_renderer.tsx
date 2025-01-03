05c57731b9c487d5261a69d979d4818630748d3b
firstYearEmployment <forwork07241998@gmail.com>
2025-01-04 00:18:43 +0900

    🐛 Headタグの中身が出力されない
    Nested Layoutsを使って、コンポーネントは利用しない

--- honox-app/app/routes/_renderer.tsx
+++ honox-app/app/routes/_renderer.tsx
@@ -1,12 +1,20 @@
 import { jsxRenderer } from 'hono/jsx-renderer'
 import Header from '../islands/common/header'
 import Footer from '../islands/common/footer'
-import Head from '../islands/common/head'
 
 export default jsxRenderer(({ children, title, frontmatter }) => {
   return (
     <html lang='ja' class="dark">
-      <Head title={title} frontmatter={frontmatter} />
+      <head>
+        <meta charset='UTF-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        {<title>{title ?? frontmatter?.title ?? 'かもしかの遊び場'}</title>}
+        {import.meta.env.PROD ? (
+          <link rel="stylesheet" href="/static/assets/style.css" />
+        ) : (
+          <link rel="stylesheet" href="/app/style.css" />
+        )}
+      </head>
       <body className="bg-teal-50">
         <Header />
         <main className="mx-4 md:mx-[16rem] lg:mx-[24rem] min-h-screen flex flex-col">
--- honox-app/app/routes/articles/_renderer.tsx
+++ honox-app/app/routes/articles/_renderer.tsx
@@ -1,29 +1,19 @@
 import { jsxRenderer } from "hono/jsx-renderer";
-import Head from "../../islands/common/head";
-import Header from "../../islands/common/header";
-import Footer from "../../islands/common/footer";
 
-export default jsxRenderer(({ children, title, frontmatter }) => {
+export default jsxRenderer(({ children, title, frontmatter, Layout }) => {
   return (
-    <html lang='ja' class="dark">
-    <Head title={title} frontmatter={frontmatter} />
-    <body className="bg-teal-50">
-      <Header />
-      <main className="mx-4 lg:mx-[16rem] xl:mx-[24rem] min-h-screen flex flex-col">
-        <div className="flex flex-col items-center my-4">
-          {frontmatter.iconUrl && (
-            <img src={frontmatter.iconUrl} alt={frontmatter.title} className='w-16 h-16 mb-2' />
-          )}
-          <h1 className="text-3xl font-bold text-center">{frontmatter.title}</h1>
-          <div className="my-2"></div>
-          <p className="text-sm text-center">{frontmatter.date}</p>
-        </div>
-        <div class="markdown bg-white my-4 p-4">
-            {children}
-        </div>
-      </main>
-      <Footer />
-    </body>
-  </html>
+    <Layout title={title} frontmatter={frontmatter}>
+      <div className="flex flex-col items-center my-4">
+        {frontmatter.iconUrl && (
+          <img src={frontmatter.iconUrl} alt={frontmatter.title} className='w-16 h-16 mb-2' />
+        )}
+        <h1 className="text-3xl font-bold text-center">{frontmatter.title}</h1>
+        <div className="my-2"></div>
+        <p className="text-sm text-center">{frontmatter.date}</p>
+      </div>
+      <div class="markdown bg-white my-4 p-4">
+        {children}
+      </div>
+    </Layout>
   );
 });
