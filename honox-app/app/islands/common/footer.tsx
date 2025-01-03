f25346025905504147a98d329278aa6e747934f1
firstYearEmployment <forwork07241998@gmail.com>
2025-01-03 16:48:11 +0900

    ✨ ヘッダーとフッターを追加する

---
+++ honox-app/app/islands/common/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-teal-50 text-black p-4 text-center">
+      <p>&copy; 2025 かもしかの遊び場. All rights reserved.</p>
+    </footer>
+  )
+}
---
+++ honox-app/app/islands/common/header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="bg-teal-50 text-black p-4">
+      <h1>
+        <a href='/' className="text-2xl font-bold">かもしかの遊び場🦌</a>
+      </h1>
+    </header>
+  )
+}
