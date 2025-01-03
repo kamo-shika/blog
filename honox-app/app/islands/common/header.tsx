6b4e03d880579194d12e98c37560af7fd2d6c19d
firstYearEmployment <forwork07241998@gmail.com>
2025-01-04 02:05:00 +0900

    🐛 GitHubアイコンが横並びになってなかったので修正
    flexで両端に配置する

--- honox-app/app/islands/common/header.tsx
+++ honox-app/app/islands/common/header.tsx
@@ -1,14 +1,15 @@
 export default function Header() {
   return (
-    
     <header className="text-black p-4 bg-white">
-      <h1>
-        <a href='/' className="text-2xl font-bold">かもしかの遊び場🦌</a>
-      </h1>
-      <div className="absolute right-4 top-4">
-        <a href="https://github.com/kamo-shika/blog" target="_blank" rel="noopener noreferrer">
-          <img src="/static/github-mark.svg" alt="GitHub" className="w-8 h-8" />
-        </a>
+      <div className="flex justify-between items-center">
+        <h1>
+          <a href='/' className="text-2xl font-bold">かもしかの遊び場🦌</a>
+        </h1>
+        <div className="flex gap-4">
+          <a href="https://github.com/kamo-shika/blog" target="_blank" rel="noopener noreferrer">
+            <img src="/static/github-mark.svg" alt="GitHub" className="w-8 h-8" />
+          </a>
+        </div>
       </div>
     </header>
   )
