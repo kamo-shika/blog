d532730f3d715e25de668018b6a1fa9e781de319
firstYearEmployment <forwork07241998@gmail.com>
2025-01-05 16:26:24 +0900

    🐛 やっぱりCSSファイルが生成されない
    デプロイ時のみもとに戻す

--- honox-app/app/routes/_renderer.tsx
+++ honox-app/app/routes/_renderer.tsx
@@ -18,7 +18,7 @@ export default jsxRenderer(({ children, title, frontmatter }) => {
         }
         {<title>{title ?? frontmatter?.title ?? 'かもしかの遊び場'}</title>}
         {import.meta.env.PROD ? (
-          <link rel="stylesheet" href="/static/assets/style/style.css" />
+          <link rel="stylesheet" href="/static/assets/style.css" />
         ) : (
           <link rel="stylesheet" href="/app/style/style.css" />
         )}
