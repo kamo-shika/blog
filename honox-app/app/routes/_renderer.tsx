80df8ae8a9594588f0b9103c1304b2679bfc5abd
firstYearEmployment <forwork07241998@gmail.com>
2025-01-05 17:11:01 +0900

    🔍️ OGPタグを仮で追加
    画像とかまだ用意してない、そのうち自動生成もする？

--- honox-app/app/routes/_renderer.tsx
+++ honox-app/app/routes/_renderer.tsx
@@ -4,19 +4,25 @@ import Footer from '../islands/common/footer'
 import { html } from 'hono/html';
 
 export default jsxRenderer(({ children, title, frontmatter }) => {
+
+  const _title = title ?? frontmatter?.title ?? 'かもしかの遊び場'
+  const _description = frontmatter?.description ?? '2022年新卒入社のバックエンドエンジニア。AWS等の資格を取得しながら、フロントエンド開発の経験を積むために本ブログを開設。技術と趣味の記事を発信していきます。'
+  const url = `https://kamoshika-playground.dev/`
   return (
     <html lang='ja' class="dark">
       <head>
+        <title>{_title}</title>
         <meta charset='UTF-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <meta name="robots" content="all" />
-        {
-          <meta 
-            name="description"  
-            content={frontmatter?.description ?? '2022年新卒入社のバックエンドエンジニア。AWS等の資格を取得しながら、フロントエンド開発の経験を積むために本ブログを開設。技術と趣味の記事を発信していきます。'}
-          />
-        }
-        {<title>{title ?? frontmatter?.title ?? 'かもしかの遊び場'}</title>}
+        <meta name="description" content={_description}/>
+        <meta property="og:title" content={_title} />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="かもしかの遊び場" />
+        <meta property="og:description" content={_description} />
+        <meta property="og:locale" content="ja_JP" />
+        <meta property="og:url" content={url} />
+        {/* スタイルシート */}
         {import.meta.env.PROD ? (
           <link rel="stylesheet" href="/static/assets/style.css" />
         ) : (
